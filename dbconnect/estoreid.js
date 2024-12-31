5d28d89ba1e985c179d20f5959a8dc3c84992561
framnz85 <clavmall.85@gmail.com>
2024-12-31 09:46:39 +0800

    commit 12-31-24 9:00 am

--- dbconnect/estoreid.js
+++ dbconnect/estoreid.js
@@ -1,4 +1,10 @@
-const estoreid1 = ["675403abc7a3632df514d5fc"];
+const estoreid1 = [
+  "66180257fd06884019423dbe",
+  "66dd4ca828a6ce0ba1a696e0",
+  "613216389261e003d696cc65",
+  "675403abc7a3632df514d5fc",
+  "67262902fc9b3d9326cd9e63",
+];
 
 const estoreid = [
   {
