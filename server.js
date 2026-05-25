d51a1fe2129eacb5833271c2374532c34f57f03d
framnz85 <clavmall.85@gmail.com>
2026-05-25 20:36:23 +0800

    commit 5-26-26 8:00 am

--- server.js
+++ server.js
@@ -8,10 +8,25 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json({ limit: "2mb" }));
-app.use(cors());
+
+const allowedOrigins = ["http://localhost:3001"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  }),
+);
 
 readdirSync("./routes").map((file) =>
-  app.use("/" + process.env.API_ROUTES, require("./routes/" + file))
+  app.use("/" + process.env.API_ROUTES, require("./routes/" + file)),
 );
 
 const port = process.env.PORT || 8000;
